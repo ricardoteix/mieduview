04afa284746935c6d5e579910e1c8f8d95c2050e
Ricardo Teixeira <ricardoteix@gmail.com>
2018-03-31 17:30:44 -0300

    novos botões e um alerta

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController  } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,20 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    public navCtrl: NavController
+    public alertCtrl: AlertController
 
-  }
+    constructor(navCtrl: NavController, alertCtrl: AlertController) {
+      this.navCtrl = navCtrl;
+      this.alertCtrl = alertCtrl;
+    }
 
+    public showAlert() {
+        let alert = this.alertCtrl.create({
+            title: 'New Friend!',
+            subTitle: 'Your friend, Obi wan Kenobi, just accepted your friend request!',
+            buttons: ['OK']
+        });
+        alert.present();
+    }
 }
